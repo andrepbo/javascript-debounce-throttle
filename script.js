f06d9e4ce0bf9a93b92ff145979c0b3f913e3de0
Andre Oliveira <andre.pbo@gmail.com>
2025-02-24 20:03:16 +0000

    Add debounce function to button click event

--- script.js
+++ script.js
@@ -11,12 +11,6 @@ function throttle(func, limit) {
     }
 }
 
-// Function that will be called on scroll
-function handleScroll() {
-    console.log("Scroll detected in:", new Date().toLocaleDateString());
-    document.getElementById("scroll-status").innerText = `Last scroll ${new Date().toLocaleTimeString()}`;
-}
-
 // Debounce function: Executes the function only after a period of inactivity
 function debounce(func, delay) {
     let timeout;
@@ -31,9 +25,25 @@ function searchQuery(query) {
     console.log(`Searching for: ${query}`)
 }
 
+// Function that will be called on scroll
+function handleScroll() {
+    console.log("Scroll detected in:", new Date().toLocaleDateString());
+    document.getElementById("scroll-status").innerText = `Last scroll ${new Date().toLocaleTimeString()}`;
+}
+
+// Function simulating an action when clicking the button
+function handleDebounceClick() {
+    console.log("Button clicked");
+    document.getElementById("debounce-status").innerText = "Action executed";
+}
+
 // Capture the input and apply the debounce
 const input = document.getElementById("search");
 input.addEventListener("input", debounce((event) => searchQuery(event.target.value), 500))
 
 // // Applies throttle to the scroll event
 window.addEventListener("scroll", throttle(handleScroll, 1000));
+
+// Debounce on button click
+const debounceButton = document.getElementById("debounce-btn");
+debounceButton.addEventListener("click", debounce(handleDebounceClick, 1000));
